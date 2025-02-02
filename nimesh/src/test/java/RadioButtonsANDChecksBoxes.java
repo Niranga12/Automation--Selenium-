f7dd5676214036e19b9deb085c6e1ca04c37fd56
Niranga12 <149963647+Niranga12@users.noreply.github.com>
2025-02-02 18:00:05 +0530

    RadioButtons1
    learn about .isSelected,if Condition,radiobuttons

---
+++ nimesh/src/test/java/RadioButtonsANDChecksBoxes.java
@@ -0,0 +1,66 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class RadioButtonsANDChecksBoxes {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void RadioButtonsANDChecksBoxe(){
+        driver =new ChromeDriver();
+        driver.manage().window().maximize();
+
+    }
+    @Test
+    public void radioTest(){
+        //1 find the default selecte radio button
+        driver.get("https://www.leafground.com/radio.xhtml;jsessionid=node016z20gm119vfl1b2swc7c1ium78606477.node0");
+
+        boolean chromeRadioOption=driver.findElement(By.id("j_idt87:console2:0")).isSelected();
+        boolean firefoxRadioOption=driver.findElement(By.id("j_idt87:console2:1")).isSelected();
+        boolean safariRadioOption=driver.findElement(By.id("j_idt87:console2:2")).isSelected();
+        boolean edgeRadioOption=driver.findElement(By.id("j_idt87:console2:3")).isSelected();
+
+        if (chromeRadioOption){
+            String chromeText=driver.findElement(By.xpath("//label[@for='j_idt87:console2:0']")).getText();
+            System.out.println("default selecte radio button is :"+chromeText);
+
+        } else if (firefoxRadioOption) {
+            String firefoxTest=driver.findElement(By.xpath("//label[@for='j_idt87:console2:1'")).getText();
+            System.out.println("default selecte radio button is : "+firefoxTest);
+
+        } else if (safariRadioOption) {
+            String safariTest=driver.findElement(By.xpath("//label[@for='j_idt87:console2:2'")).getText();
+            System.out.println("default selecte radio button is : "+safariTest);
+
+        } else if (edgeRadioOption) {
+            String edgeTest=driver.findElement(By.xpath("//label[@for='j_idt87:console2:3'")).getText();
+            System.out.println("default selecte radio button is : "+edgeTest);
+
+
+        }
+
+
+        //2 select the age group(if not selected)
+        WebElement myAgeGroup=driver.findElement(By.id("j_idt87:age:0"));
+        boolean ischeck=myAgeGroup.isSelected();
+        if (ischeck!=true){
+            //myAgeGroup.click();
+            driver.findElement(By.xpath("//label[@for='j_idt87:age:0']")).click();
+
+        }
+
+
+    }
+    @Test
+    public void checksTest(){
+
+
+    }
+
+
+
+}
